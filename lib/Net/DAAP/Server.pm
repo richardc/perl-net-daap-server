fedb66b1dff6526f421456cb609a969c61589bf3
tomi <tomi@c56e198b-7db7-0310-b3ab-8e406f684e33>
2004-10-02 21:19:23 +0000

    Optionally cache the Track objects by filename, and update the cache based on the mtime of the file. Speeds startup when dealing with lots of files.
    git-svn-id: file:///web/svn.unixbeard.net/repo/richardc/perl/Net-DAAP-Server@2887 c56e198b-7db7-0310-b3ab-8e406f684e33

--- lib/Net/DAAP/Server.pm
+++ lib/Net/DAAP/Server.pm
@@ -3,6 +3,8 @@ use strict;
 use warnings;
 use Net::DAAP::Server::Track;
 use File::Find::Rule;
+use Storable ();
+
 use base qw( Net::DMAP::Server );
 our $VERSION = '0.03';
 
@@ -10,12 +12,87 @@ sub protocol { 'daap' }
 
 sub default_port { 3689 }
 
+# the filename we use to cache in
+__PACKAGE__->mk_accessors('cache_file');
+
+# load the cache from disk, if we have a filename
+sub load_cache {
+    my $self = shift;
+
+    # no cache unless we're asked for one
+    return {} unless $self->cache_file;
+
+    my $cache = eval { Storable::retrieve( $self->cache_file ) } || {};
+    warn "Error loading cache: $!\n" if $!;
+
+    # we just loaded it, it's clean
+    $cache->{_updated} = 0;
+
+    return $cache;
+}
+
+# get a Net::DAAP::Server::Track object from a filename, through the cache
+# if possible, off disk if not.
+sub get_cached_file {
+    my ($self, $cache, $filename) = @_;
+
+    # skip annoying mac metadata that takes ages to scan
+    return if $filename =~ /\.AppleDouble/;
+  
+    # useful.
+    my @s = stat($filename);
+
+    # it there's a cached copy and it's up to date..
+    if (my $c = $cache->{$filename}) {
+        return $c->{track} if ($c->{mtime} == $s[9]);
+    }
+
+    # the track is either not cached, or not up to date.
+    my $track = Net::DAAP::Server::Track->new_from_file( $filename );
+
+    # we changed the cache, mark it as dirty
+    $cache->{_updated} = 1;
+
+    # store the mtime as well as the track, so we can notice changes
+    $cache->{$filename} = {
+        track => $track,
+        mtime => $s[9],
+    };
+  
+    return $track;
+}
+
+# save the cache to disk, if we have a filename
+sub save_cache {
+    my ($self, $cache) = @_;
+    return unless $self->cache_file;
+
+    # if the cache isn't dirty, don't save it.
+    return unless $cache->{_updated};
+    
+    Storable::store( $cache, $self->cache_file );
+}
+
 sub find_tracks {
     my $self = shift;
-    for my $file ( find name => [ "*.mp3", "*.m4p", "*.m4a" ], in => $self->path ) {
-        my $track = Net::DAAP::Server::Track->new_from_file( $file ) or next;
+
+    #print STDERR "loading cache..\n";
+    my $cache = $self->load_cache;
+
+    #print STDERR "scanning for files..\n";
+    my @files = find name => [ "*.mp3", "*.m4p", "*.m4a" ], in => $self->path;
+
+    #print STDERR "adding files..\n";
+    for my $file ( @files ) {
+        my $track = $self->get_cached_file( $cache, $file ) or next;
         $self->tracks->{ $track->dmap_itemid } = $track;
     }
+
+    #print STDERR "saving cache..\n";
+    $self->save_cache($cache);
+
+    #print STDERR "ready..\n";
+    
 }
 
 sub server_info {
